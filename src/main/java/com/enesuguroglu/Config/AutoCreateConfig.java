13d0f48b4e6349d6e20891507c5539d242e797d4
euguroglu <e.uguroglu@yahoo.com>
2021-12-13 23:27:41 +0300

    change

--- src/main/java/com/enesuguroglu/Config/AutoCreateConfig.java
+++ src/main/java/com/enesuguroglu/Config/AutoCreateConfig.java
@@ -3,21 +3,16 @@ package com.enesuguroglu.Config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.support.KafkaHeaders;
 
 
 
 @Configuration
 public class AutoCreateConfig {
 
-
-    @KafkaListener(topicPattern = "kafka.*",groupId = "test-grp")
     @Bean
     public void getTopic(){
-        String topic = KafkaHeaders.RECEIVED_TOPIC;
-        NewTopic deadlettertopic = TopicBuilder.name(String.format("%s_deadletter",topic))
+        NewTopic deadlettertopic = TopicBuilder.name("trial")
                 .partitions(1)
                 .replicas(1)
                 .build();
